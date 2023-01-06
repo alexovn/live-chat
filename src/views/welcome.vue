b697750084e74dda1f16f242fc95a48d889ad14c
Nikita Alexov <polarbeararmor@gmail.com>
2023-01-06 22:14:44 +0300

    change naming in welcome screen

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -2,7 +2,7 @@
   <div class="welcome container">
     <p>Welcome</p>
     <div v-if="showLogin">
-      <h2>Login</h2>
+      <h2>Login to the chatroom</h2>
       <LoginForm @login="enterChatroom" />
       <p>
         No account yet?
@@ -13,7 +13,7 @@
       </p>
     </div>
     <div v-else>
-      <h2>Sign up</h2>
+      <h2>Sign up to the chatroom</h2>
       <SignupForm @signup="enterChatroom" />
       <p>
         Already registered?
